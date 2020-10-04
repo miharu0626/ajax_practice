c92fda9748802ef15e612cfaa4ba62b7d8bcee79
miharu0626 <miharu.pearls@gmail.com>
2020-10-04 22:51:10 +0900

    ルーティング実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
+  root 'recipes#index'
+  resources :recipes, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
